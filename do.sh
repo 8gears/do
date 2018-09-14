d616a7c52b2efd8646a0205ff8fd9b71a981cd44
Vadim Bauer <bauer.vadim@gmail.com>
2018-09-14 17:42:35 +0200

    added hidden functions

--- do.sh
+++ do.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh -eu
-# Do - The Simplest Build Tool on Earth. See https://github.com/8gears/do
+# Do - The Simplest Build Tool on Earth.
+# Documentation and examples see https://github.com/8gears/do
 
 function build() {
 	echo "I am ${FUNCNAME[0]}ing"
@@ -17,6 +18,10 @@ function all() {
 	build && test && deploy
 }
 
-"$@"
+function _hidden() {
+	echo "I am a hidden task and won't appear in the usage desciption because I start with an _ (underscore). If you know me you can still call me directly"
+}
 
-let $# || echo "Usage:\n\t./do.sh ($(compgen -A function | paste -sd '|' -))"
+"$@" # <- execute the task
+
+let $# || echo "Usage:\n\t./do.sh ($(compgen -A function | grep ^[^_] | paste -sd '|' -))"
